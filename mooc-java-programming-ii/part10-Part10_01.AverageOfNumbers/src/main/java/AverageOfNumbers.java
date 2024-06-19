53b12b27559e7939ec046d0632f2b8e5498ca586
ebaek88 <euihyunbaek88@knou.ac.kr>
2024-06-19 21:45:32 +0900

    Added solutions up to mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers

--- mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-ii/part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfNumbers {
@@ -7,6 +8,28 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
+        List<String> numberList = new ArrayList<>(); //list for storing numbers
+        
+        //starts the loop to store numbers in the list
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while(true) {
+            String input = scanner.nextLine();
+            if(input.equals("end")) {
+                break;
+            }
+
+            numberList.add(input);
+        }
+
+        //calculating the average of numbers in the list using stream
+        //for stream, you don't put access modifier...?
+        double average = numberList.stream()
+            .mapToInt(s -> Integer.valueOf(s))
+            .average()
+            .getAsDouble();
+
+        // printing the average
+        System.out.println("average of the numbers: " + average);
 
     }
 }
--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
@@ -7,6 +8,53 @@ public class AverageOfSelectedNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // toteuta ohjelmasi tänne
+        List<String> numberList = new ArrayList<>(); //list for storing numbers
+        
+        //starts the loop to store numbers in the list
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while(true) {
+            String input = scanner.nextLine();
+            if(input.equals("end")) {
+                break;
+            }
 
-    }
-}
+            numberList.add(input);
+        }//while
+
+        double average = 0; // variable for storing the averages
+        String option;
+
+        while(true) {
+            //ask to choose the option : the average of the negative numbers or the positive numbers
+            System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+            option = scanner.nextLine();
+            
+            //if n is entered, calculate the average of the negative numbers
+            if(option.equals("n")) {
+                average = numberList.stream()
+                    .mapToInt(element -> Integer.valueOf(element))
+                    .filter(number -> number < 0)
+                    .average()
+                    .getAsDouble();
+
+                break;
+            } else if(option.equals("p")) { //if p is entered, calculate the average of the positive numbers
+                average = numberList.stream()
+                    .mapToInt(element -> Integer.valueOf(element))
+                    .filter(number -> number > 0)
+                    .average()
+                    .getAsDouble();
+
+                break;
+            } else {
+                System.out.println("Please select the correct option.");
+                continue;
+            }
+
+        }//while
+
+        //print the average
+        System.out.println("Average of the " + (option.equals("n") ? "negative" : "positive") + " numbers: " + average);
+
+    }//main
+}//class
