2d44cffd876318911f8c0b3c8f781a94e6733bbe
ebaek88 <euihyunbaek88@knou.ac.kr>
2024-06-20 20:51:58 +0900

    Added solutions up to mooc-java-programming-ii/part10-Part10_03.PositiveNumbers

--- mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -8,7 +9,22 @@ public class PositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(7);
+        list.add(4);
+        list.add(2);
+        list.add(6);
+        List<Integer> result = positive(list);
+        System.out.println(result);
+    }
 
+    public static List<Integer> positive(List<Integer> numbers) {
+        List<Integer> values = numbers.stream()
+            .filter(value -> value > 0)
+            // .collect(Collectors.toCollection(() -> new ArrayList<>()));
+            .collect(Collectors.toList());
+        return values;
     }
 
 }
