751b1d61ef30e922caa742d15229151c209dd782
ebaek88 <euihyunbaek88@knou.ac.kr>
2024-07-08 23:07:51 +0900

    Added solutions up to mooc-java-programming-ii/part11-Part11_09.TheThreePackages

--- mooc-java-programming-ii/part11-Part11_08.FirstPackages/src/main/java/mooc/logic/ApplicationLogic.java
+++ mooc-java-programming-ii/part11-Part11_08.FirstPackages/src/main/java/mooc/logic/ApplicationLogic.java
@@ -11,11 +11,9 @@ public class ApplicationLogic {
   }
 
   public void execute(int times) {
-    for (int i = 0; i < times; i += 1) {
-      System.out.println("Application logic is working"); // Prints the string "Application logic is working" the number
-                                                          // of times that is indicated by the variable times
-      ui.update(); // call the update() method of the object that was received as a constructor
-      // parameter
+    for (int i = 0; i < times; i++) {
+      System.out.println("Application logic is working");
+      ui.update();
     }
   }
 }
